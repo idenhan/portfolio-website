be69ef643aba9fb66422c60b94bc3e93df511ad9
Iden Han <idenhan129@gmail.com>
2020-11-07 22:51:54 +0900

    Updates

---
+++ precache-manifest.cfb5445fb9cad5c16a1e7f6d60b74759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c917afd248e26ca6f84ed71d0e57e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "484bfff0dcc458df6f1d",
+    "url": "/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "ceea2f6bf768a51e5b79",
+    "url": "/static/css/main.77850476.chunk.css"
+  },
+  {
+    "revision": "484bfff0dcc458df6f1d",
+    "url": "/static/js/2.85be9b0b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.85be9b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceea2f6bf768a51e5b79",
+    "url": "/static/js/main.84282e47.chunk.js"
+  },
+  {
+    "revision": "4a76540c0c6e9e3d740e",
+    "url": "/static/js/runtime-main.335f7064.js"
+  }
+]);
